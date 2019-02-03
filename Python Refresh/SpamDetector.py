f52ee5c5ef246ebb4b05527681d6009476e966ca
Daniel Crouch <d_crouch@outlook.com>
2019-02-03 20:11:14 +0000

    Vecorization complete

--- Python Refresh/SpamDetector.py
+++ Python Refresh/SpamDetector.py
@@ -1,10 +1,12 @@
 import os
 import glob
-from functools import reduce
-from nltk.corpus import names
 from FunctionTimerCode import time_func
+from nltk.corpus import names
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+all_names = set(names.words())
 
 def get_text_files(file_path, encoding='UTF-8'):
     """Return list of all text files in path"""
@@ -14,35 +16,35 @@ def get_text_files(file_path, encoding='UTF-8'):
             file_list.append(infile.read())
     return file_list
 
-def is_letter_only(astr):
+def letter_only(astr):
     return astr.isalpha()
 
-def is_person_name(astr):
+def person_name(astr):
     return astr in names.words()
-
-path1 = '/Users/DC/Desktop/Python Machine Learning/Python Machine Learning by Example/enron1/spam/'
-path2 = '/Users/DC/Desktop/Python Machine Learning/Python Machine Learning by Example/enron1/ham/'
-spam_emails = get_text_files(path1, 'ISO-8859-1')
-ham_emails = get_text_files(path2, 'ISO-8859-1')
-all_names = set(names.words()) # Set of common names for reference.
 
 def stem_word(word):
     """Reduces word to stem, base or root form."""
     porter_stemmer = PorterStemmer()
     return porter_stemmer.stem(word)
 
-#####rework for stem not lemmatizer#####
-def stem_doc(text_doc):
-    """Reduces each word in the text documment to their word stem, base or root form."""
-    lemmatizer = WordNetLemmatizer() 
-    # Stemmers remove morphological affixes from words, leaving only the word stem.
-    return ' '.join([lemmatizer.lemmatize(word.lower()) for word in text_doc.split()])
+def clean_doc(text_doc):
+    """Return simplifed text_doc with:
+        - All lower
+        - All alphabetical
+        - Word stemmed"""
+    return ' '.join([stem_word(word.lower()) 
+                for word in text_doc.split()
+                if letter_only(word)
+                and word not in all_names])
 
 
+path1 = '/Users/DC/Desktop/Python Machine Learning/Python Machine Learning by Example/enron1/spam/'
+path2 = '/Users/DC/Desktop/Python Machine Learning/Python Machine Learning by Example/enron1/ham/'
+spam_emails = [clean_doc(doc) for doc in get_text_files(path1, 'ISO-8859-1')]
+ham_emails = [clean_doc(doc) for doc in get_text_files(path2, 'ISO-8859-1')]
+vectorizer = CountVectorizer(stop_words="english", max_features=500)
+vectorizer.fit(spam_emails + ham_emails)
+term_spam_emails = vectorizer.transform(spam_emails)
+term_ham_emails = vectorizer.transform(ham_emails)
 
-    
-    
-    
-
-
-print(stem_word('walking'))
+print(term_ham_emails.toarray())
